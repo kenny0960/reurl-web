6bbaf7087f8a563503bcaee383727ccba5996f3f
Kenny <kenny_hung@mail.chungyo.net>
2023-06-06 12:09:15 +0800

    安裝 bootstrap-icon 套件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
 </template>
 
 <style lang="scss">
+@import "~bootstrap-icons/font/bootstrap-icons.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
