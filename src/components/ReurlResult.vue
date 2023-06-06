60f2b6532ccc719f36a0528fb22213d5f0eae29d
Kenny <kenny_hung@mail.chungyo.net>
2023-06-06 18:01:16 +0800

    顯示驗證結果畫面

---
+++ src/components/ReurlResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    v-if="!isUrlValid"
+    class="mt-2 p-2 text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3"
+  >
+    <i class="bi bi-exclamation-triangle" />
+    請輸入網址。
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-facing-decorator";
+import reurlModule from "@/store/modules/reurlModule";
+
+@Component
+export default class ReurlResult extends Vue {
+  get isUrlValid(): boolean {
+    return reurlModule.isUrlValid;
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,21 @@
   <div class="vw-100 mx-5">
     <ReurlTitle />
     <ReurlInput />
+    <ReurlResult />
   </div>
 </template>
 
 <script lang="ts">
 import ReurlInput from "@/components/ReurlInput.vue";
 import ReurlTitle from "@/components/ReurlTitle.vue";
+import ReurlResult from "@/components/ReurlResult.vue";
 import { Component, Vue } from "vue-facing-decorator";
 
 @Component({
   components: {
     ReurlTitle,
     ReurlInput,
+    ReurlResult,
   },
 })
 export default class HomeView extends Vue {}
