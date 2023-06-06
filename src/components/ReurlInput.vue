c3665b89a39b11ed76402cb7b7d4ecd29f47c016
Kenny <kenny_hung@mail.chungyo.net>
2023-06-06 12:01:27 +0800

    Hello World

---
+++ src/components/ReurlInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="reurl-input">Hello World</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ReurlInput",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.reurl-input {
+}
+</style>
