315299213728c0bf7f5002564d2d53b015ac1844
Kenny <kenny_hung@mail.chungyo.net>
2023-06-08 14:45:39 +0800

    更新 V1.0.0 版本
    - 增加 LOGO 圖片
    - 顯示更新日誌

---
+++ src/consts/UPDATE_LOGS.ts
@@ -0,0 +1,11 @@
+import UpdateLog from "@/interfaces/UpdateLog";
+
+const UPDATE_LOGS: UpdateLog[] = [
+  {
+    version: "1.0.0",
+    date: "20230608 1500",
+    messages: ["增加 LOGO 圖片", "顯示更新日誌"],
+  },
+];
+
+export default UPDATE_LOGS;
