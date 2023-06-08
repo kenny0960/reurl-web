c2c58b47c8fe5e54e2357380102f39adb91585de
Kenny <kenny_hung@mail.chungyo.net>
2023-06-08 14:42:11 +0800

    顯示更新日誌

--- src/components/ReurlFooter.vue
+++ src/components/ReurlFooter.vue
@@ -1,17 +1,29 @@
 <template>
   <footer class="py-3">
-    <span class="small text-secondary">ⓚ design © V{{ version }}</span>
+    <span class="small text-secondary" :title="updateLogs"
+      >ⓚ design © V{{ version }}</span
+    >
   </footer>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-facing-decorator";
 import * as PackageJson from "@/../package.json";
+import UpdateLog from "@/interfaces/UpdateLog";
+import UPDATE_LOGS from "@/consts/UPDATE_LOGS";
 
 @Component
 export default class ReurlFooter extends Vue {
   get version(): string {
     return PackageJson.version;
+  }
+
+  get updateLogs(): string {
+    return UPDATE_LOGS.slice(0, 5).map(this.stringifyUpdateLog).join("\n");
+  }
+
+  private stringifyUpdateLog(updateLog: UpdateLog): string {
+    return `v${updateLog.version} ${updateLog.date} ${updateLog.messages}`;
   }
 }
 </script>
